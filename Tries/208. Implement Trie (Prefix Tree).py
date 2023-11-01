c8b61018a564140c416b9b165eb9d0e488065236
Priyank Thakur <priyankthakur3@live.com>
2023-11-01 15:44:03 -0400

    Trie

---
+++ Tries/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,44 @@
+class TrieNode:
+
+    def __init__(self):
+        self.char = [None] * 26
+        self.is_end = False
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            char_ascii = ord(char) - ord('a')
+            if not curr.char[char_ascii]:
+                curr.char[char_ascii] = TrieNode()
+            curr = curr.char[char_ascii]
+        curr.is_end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            char_ascii = ord(char) - ord('a')
+            if not curr.char[char_ascii]:
+                return False
+            curr = curr.char[char_ascii]
+        return curr.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            char_ascii = ord(char) - ord('a')
+            if not curr.char[char_ascii]:
+                return False
+            curr = curr.char[char_ascii]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)

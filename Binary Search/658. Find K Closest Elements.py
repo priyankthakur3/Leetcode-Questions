9c840428ba37eed4cff45f662a27af68119b6fc6
Priyank Thakur <priyankthakur3@live.com>
2024-09-17 01:49:43 -0400

    comment

--- Binary Search/658. Find K Closest Elements.py
+++ Binary Search/658. Find K Closest Elements.py
@@ -1,12 +1,14 @@
-class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        left, right = 0, len(arr) - k
-    
-        while left < right:
-            mid = (left + right) // 2
-            if x - arr[mid] > arr[mid + k] - x:
-                left = mid + 1
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        lo, hi = 0, len(arr)-k
+        while lo < hi:
+            mid = (lo + hi)//2
+            #  x - arr[mid] > arr[mid + k] - x in the code is used to 
+            # determine whether the element at the current mid index 
+            # is farther away from x than the element at mid + k 
+            # (the last element of the current window of k elements
+            if x-arr[mid] > arr[mid+k]-x:
+                lo = mid + 1
             else:
-                right = mid
-
-        return arr[left:left + k]
+                hi = mid
+        return arr[lo:lo+k]
